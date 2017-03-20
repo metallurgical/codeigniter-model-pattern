41a3bf7f777069264f142832b36cad0d9cce5068
Norlihazmey Ghazali <norlihazmey89@gmail.com>
2017-03-20 12:46:43 +0800

    Create User_model.php

---
+++ application/models/User_model.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH.'/models/Model_app.php';
+
+// extend Model app instead of CI_Model
+// Dont worry, Model_app is a derived class of CI_Model
+class User_model extends Model_app {
+    // specify table's name, must be same with database's table
+    protected $table = 'users';
+
+    function __construct()
+    {
+        // Call the Model constructor
+        parent::__construct();
+    } 
+
+}
